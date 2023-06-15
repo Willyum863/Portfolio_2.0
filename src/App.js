eb19369476239aba804c26fec90c2a3e150c2c4b
William Combee <william.combee@yahoo.com>
2023-06-15 14:11:45 -0500

    added imports and page to App.js

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import { useState } from 'react';
+import Header from './components/Header';
+import Project from './components/Container';
+import Footer from './components/Footer';
+
+function App() {
+
+  const [currentlySelected, setCurrentlySelected] = useState('About Me');
+
+  return (
+    <>
+      <Header 
+        currentlySelected={currentlySelected}
+        setCurrentlySelected={setCurrentlySelected}
+      />
+      <Project currentlySelected={currentlySelected} />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
